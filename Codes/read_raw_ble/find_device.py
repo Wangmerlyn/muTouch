0b7edd118f35bc0a57c60eb79d1447959c571a43
Siyuan Wang <wsy0227@SiyuandeMacBook-Air.local>
2023-12-26 14:49:31 +0800

    add docstring for find_device.py

--- Codes/read_raw_ble/find_device.py
+++ Codes/read_raw_ble/find_device.py
@@ -1,3 +1,6 @@
+"""
+Find all the devices that are available to connect to.
+"""
 import asyncio
 from bleak import discover
 
@@ -7,21 +10,6 @@ async def run():
     for d in devices:
         print(d)
 
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
-
-"""
-import asyncio
-from bleak import BleakClient
-
-address = "02ECCB4C-BA71-4726-8F0D-411F5CFA5246"
-MODEL_NBR_UUID = "00002a24-0000-1000-8000-00805f9b34fb"
-
-async def run(address):
-    async with BleakClient(address) as client:
-        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-        print("Model Number: {0}".format("".join(map(chr, model_number))))
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run(address))
-"""
