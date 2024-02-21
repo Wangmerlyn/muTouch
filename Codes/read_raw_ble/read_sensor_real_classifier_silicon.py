ba2e71bb1b0db51ea22314dc995b4fa2611ddb41
Wangmerlyn <wsy0227@sjtu.edu.cn>
2024-02-21 15:36:01 +0800

    change test file output path

--- Codes/read_raw_ble/read_sensor_real_classifier_silicon.py
+++ Codes/read_raw_ble/read_sensor_real_classifier_silicon.py
@@ -8,6 +8,7 @@ import datetime
 import atexit
 import time
 import numpy as np
+from Codes.read_raw_ble.utils.misc import format_current_time
 from classification import classify
 from classification.classify import load_label_encoder, load_net, load_svc, classify
 from bleak import BleakClient
@@ -17,22 +18,31 @@ import pandas as pd
 import atexit
 from collections import deque
 from utils.read_files import find_latest_file_with_prefix_and_suffix
+
 # Nordic NUS characteristic for RX, which should be writable`
 UART_RX_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 # Nordic NUS characteristic for TX, which should be readable
 UART_TX_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+
+# For user and sample info
+
+user_id = "02"
+task = "silicon"
+gesture_name = "test"
+facing = "0"
+
 num = 3
 sensors = np.zeros((num, 3))
 result = []
-offset = np.zeros((num,3))
-scale = np.ones((num,3))
+offset = np.zeros((num, 3))
+scale = np.ones((num, 3))
 threshold = 0.3
 env_delay = 16
 window_size = 64
 env_mag = np.zeros((3))
 near_mag = False
-readings_queue = deque(np.zeros(3),maxlen=window_size)
-env_readings_queue = deque(np.zeros(3),maxlen=env_delay)
+readings_queue = deque(np.zeros(3), maxlen=window_size)
+env_readings_queue = deque(np.zeros(3), maxlen=env_delay)
 alpha = 0.5
 filtered_sensors = np.zeros((num))
 filter_alpha = 0.5
@@ -42,26 +52,23 @@ res = "None"
 #     'Time Stamp', 'Sensor 1', 'Sensor 2', 'Sensor 3', 'Sensor 4', 'Sensor 5',
 #     'Sensor 6', 'Sensor 7', 'Sensor 8', 'Sensor 9', 'Sensor 10'
 # ]
-name = [
-    'Time Stamp', 'Sensor 1', 'Sensor 2', 'Sensor 3'
-]
+name = ["Time Stamp", "Sensor 1", "Sensor 2", "Sensor 3"]
 # name = ['Time Stamp', 'Sensor 1', 'Sensor 2',
 # 'Sensor 3', 'Sensor 4', 'Sensor 5', 'Sensor 6']
 
 
 def distance(b_1, b_0, p=1):
-    return np.sum(np.abs(b_0-b_1)**p)**(1/p)
+    return np.sum(np.abs(b_0 - b_1) ** p) ** (1 / p)
+
 
 @atexit.register
 def clean():
     print("Output csv")
     test = pd.DataFrame(columns=name, data=result)
-    gesture_name = "01-23_live"
-    if not os.path.exists(f"datasets/{gesture_name}"):
-        os.makedirs(f"datasets/{gesture_name}")
-    test.to_csv(
-        f"datasets/{gesture_name}/{gesture_name}-{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.csv"
-    )
+    gesture_path = f"datasets/{user_id}/{task}/{gesture_name}"
+    if not os.path.exists(gesture_path):
+        os.makedirs(gesture_path)
+    test.to_csv(f"{gesture_path}/{gesture_name}-{facing}-{format_current_time()}.csv")
     print("Exited")
 
 
@@ -76,43 +83,55 @@ def notification_handler(sender, data):
     global res
     current = [datetime.datetime.now()]
     for i in range(num):
-        sensors[i, 0] = struct.unpack('f', data[12 * i:12 * i + 4])[0]
-        sensors[i, 1] = struct.unpack('f', data[12 * i + 4:12 * i + 8])[0]
-        sensors[i, 2] = struct.unpack('f', data[12 * i + 8:12 * i + 12])[0]
+        sensors[i, 0] = struct.unpack("f", data[12 * i : 12 * i + 4])[0]
+        sensors[i, 1] = struct.unpack("f", data[12 * i + 4 : 12 * i + 8])[0]
+        sensors[i, 2] = struct.unpack("f", data[12 * i + 8 : 12 * i + 12])[0]
 
-        sensors[i,:] = (sensors[i, :] - offset[i])/scale[i]
+        sensors[i, :] = (sensors[i, :] - offset[i]) / scale[i]
 
-        print(f"Sensor {i+1}: {sensors[i, 0]:.6f}, {sensors[i, 1]:.6f}, {sensors[i, 2]:.6f}")
-        current.append("(" + str(sensors[i, 0]) + ", " + str(sensors[i, 1]) +
-                       ", " + str(sensors[i, 2]) + ")")
-    filtered_sensors = (1-filter_alpha)*sensors + (filter_alpha)*filtered_sensors
-    if (distance(filtered_sensors[0,:], filtered_sensors[-1,:], 2)>threshold):
+        print(
+            f"Sensor {i+1}: {sensors[i, 0]:.6f}, {sensors[i, 1]:.6f}, {sensors[i, 2]:.6f}"
+        )
+        current.append(
+            "("
+            + str(sensors[i, 0])
+            + ", "
+            + str(sensors[i, 1])
+            + ", "
+            + str(sensors[i, 2])
+            + ")"
+        )
+    filtered_sensors = (1 - filter_alpha) * sensors + (filter_alpha) * filtered_sensors
+    if distance(filtered_sensors[0, :], filtered_sensors[-1, :], 2) > threshold:
         print("YES")
         print(f"envmag is {env_mag}")
         near_mag = True
         env_mag = env_readings_queue[0]
         readings_queue.append(filtered_sensors.copy())
-        if len(readings_queue)==window_size:
+        if len(readings_queue) == window_size:
             # res = classify(net ,svc, np.array(readings_queue), label_encoder)
             # print(f"result is {res}")
             print("Window full")
     else:
-        if len(readings_queue)>min_window_len:
+        if len(readings_queue) > min_window_len:
             print(np.array(readings_queue))
-            res = classify(net ,svc, np.array(readings_queue), label_encoder)
+            res = classify(net, svc, np.array(readings_queue), label_encoder)
             print(f"result is {res}")
-        elif len(readings_queue)>1:
+        elif len(readings_queue) > 1:
             res = "TOO Short"
-            
+
         print("NO")
         print(f"last result is {res}")
         env_readings_queue.append(filtered_sensors)
         near_mag = False
         readings_queue.clear()
         # env_mag = alpha*env_mag+(1-alpha)*readings_queue[0]
-    for i in range(num):
-        print(f"Filtered Sensor {i+1}: {filtered_sensors[i, 0]:.6f}, {filtered_sensors[i, 1]:.6f}, {filtered_sensors[i, 2]:.6f}")
-    
+    # hide the filtered sensor since that's too much info on the terminal
+    # for i in range(num):
+    #     print(
+    #         f"Filtered Sensor {i+1}: {filtered_sensors[i, 0]:.6f}, {filtered_sensors[i, 1]:.6f}, {filtered_sensors[i, 2]:.6f}"
+    #     )
+
     # battery_voltage = struct.unpack('f', data[12 * num: 12 * num + 4])[0]
     # print("Battery voltage: " + str(battery_voltage))
     print("############")
@@ -134,27 +153,31 @@ async def run(address, loop):
 
 async def main():
     global address
-    await asyncio.gather(
-        asyncio.create_task(run(address, asyncio.get_event_loop())))
+    await asyncio.gather(asyncio.create_task(run(address, asyncio.get_event_loop())))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # address = ("D4:6B:83:ab:C5:F2")  # circle board
     # address = ("C2:3C:D5:6E:35:0A")  # joint board 2
-    address = "E8:71:7E:9D:FB:53" # 3 sensor board
+    address = "E8:71:7E:9D:FB:53"  # 3 sensor board
     num = 3
     # find corresponding calibration files
     file_folder = "mytest"
-    offset_path = os.path.join(file_folder,
-        find_latest_file_with_prefix_and_suffix(file_folder,"offset-"))
-    scale_path = os.path.join(file_folder,
-        find_latest_file_with_prefix_and_suffix(file_folder,"scale-"))
+    offset_path = os.path.join(
+        file_folder, find_latest_file_with_prefix_and_suffix(file_folder, "offset-")
+    )
+    scale_path = os.path.join(
+        file_folder, find_latest_file_with_prefix_and_suffix(file_folder, "scale-")
+    )
     offset = np.load(offset_path)
     scale = np.load(scale_path)
 
     # load classifier
-    net = load_net("Codes/read_raw_ble/models/net_silicon", "3_sensor_silicon_", ".pth")
-    label_encoder = load_label_encoder("Codes/read_raw_ble/models/label_encoder_silicon", "label_encoder_silicon-", ".joblib")
-    svc = load_svc("Codes/read_raw_ble/models/svc_silicon", "svc_silicon-", ".joblib")
+    model_path = f"Codes/read_raw_ble/models/{user_id}/{task}"
+    net = load_net(f"{model_path}/net_silicon", "3_sensor_silicon_", ".pth")
+    label_encoder = load_label_encoder(
+        f"{model_path}/label_encoder_silicon", "label_encoder_silicon-", ".joblib"
+    )
+    svc = load_svc(f"{model_path}/svc_silicon", "svc_silicon-", ".joblib")
     print("loading done")
     asyncio.run(main())
