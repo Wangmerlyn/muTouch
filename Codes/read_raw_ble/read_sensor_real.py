af6cb9bc8de58bd6eaa022d5d3f559d28bdda55e
Wangmerlyn <wsy0227@sjtu.edu.cn>
2024-02-21 14:27:14 +0800

    stop tracking net

--- Codes/read_raw_ble/read_sensor_real.py
+++ Codes/read_raw_ble/read_sensor_real.py
@@ -54,8 +54,8 @@ def distance(b_1, b_0, p=1):
 def clean():
     print("Output csv")
     test = pd.DataFrame(columns=name, data=result)
-    folder_name = "02-17_facetouching"
-    gesture_name = "02-17_no-touch"
+    folder_name = "02-20_facetouching"
+    gesture_name = "02-20_no-touching"
     if not os.path.exists(f"datasets/{folder_name}"):
         os.makedirs(f"datasets/{folder_name}")
     if not os.path.exists(f"datasets/{folder_name}/{gesture_name}"):
