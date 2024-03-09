b34074e2e1c09d6de92a7357712a924c1381e2dc
Wangmerlyn <wsy0227@sjtu.edu.cn>
2024-03-09 21:39:23 +0800

    add data for 11

--- Codes/read_raw_ble/read_sensor_real.py
+++ Codes/read_raw_ble/read_sensor_real.py
@@ -25,9 +25,9 @@ UART_TX_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 
 # For user and sample info
 
-user_id = "11"
-task = "face_touching"
-gesture_name = "pretrain"
+user_id = "01"
+task = "silicon"
+gesture_name = "backward"
 num = 3
 sensors = np.zeros((num, 3))
 result = []
